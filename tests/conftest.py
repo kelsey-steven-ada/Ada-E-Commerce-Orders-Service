34155cc91081f15cc6029f323b81c4280d2b69b5
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2026-07-31 13:37:38 -0700

    Update default region to us-west-2

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 @pytest.fixture
 def aws_credentials():
-    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+    os.environ["AWS_DEFAULT_REGION"] = "us-west-2"
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
